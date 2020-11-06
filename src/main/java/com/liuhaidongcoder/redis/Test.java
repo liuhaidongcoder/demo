d94ba8dd414543ec98b242d5c38889998fa8b00d
liuhaidong <liuhaidong@58.com>
2020-11-06 16:29:58 +0800

    updated

--- src/main/java/com/liuhaidongcoder/redis/Test.java
+++ src/main/java/com/liuhaidongcoder/redis/Test.java
@@ -11,5 +11,6 @@ public class Test implements  Cloneable {
     public static void main(String[] args) throws CloneNotSupportedException, NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
         System.out.printf("%X",new String(messageDigest.digest("hello".getBytes())));
+
     }
 }
